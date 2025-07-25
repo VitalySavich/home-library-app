a10511ab005b0c646c07a713d4397f333e1acaeb
Vitali Savich <sviprogrammer@rambler.ru>
2025-07-25 15:06:56 +0300

    Initial commit

---
+++ src/main/java/com/vssoftdev/home_library_app/HomeLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.vssoftdev.home_library_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeLibraryAppApplication.class, args);
+	}
+
+}
